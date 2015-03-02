d826fee5cbb663aac74458f7a5d8a7a978017e0e
class <class@CST_N417_02.cst.citytech.cuny.edu>
2015-03-02 16:29:09 -0500

    Startinglab2

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,39 @@
+ //This program calculates the sales price of an
+ // item that is regularly priced at $59, with 
+ // a 20 percent discount subtracted
+ 
+
+package javaapplication1;
+
+/**
+ *
+ * @author class
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Variable to hold the regular price, the
+        // amount of a discount, and the sale price.
+        
+        double regularPrice = 59.0;
+        double discount;
+        double salePrice;
+        
+        
+        // Calculate the amount of a 20% discount
+        discount = regularPrice * 0.2;
+        
+        
+        // Calculate the price by subtracting
+        // the discount from the regular price.
+        salesPrice = regularPrice - discount;
+        
+        //Display the realize
+                
+                
+    }
+    
+}
